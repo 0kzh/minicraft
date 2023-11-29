90562327ccb4cf94b40d68e30100c42779f2a1a5
Kelvin Zhang <me@kelvinzhang.ca>
2023-11-29 13:47:11 -0500

    attempt build fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
+import { resolve } from "path";
+
 import { defineConfig } from "vite";
 import { comlink } from "vite-plugin-comlink";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  alias: {
+    "~": resolve(__dirname, "src"),
+  },
   plugins: [comlink()],
+  worker: {
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
+    plugins: [comlink()],
+  },
 });
