f18759778126a3899d91dca97fba19289f6f501a
Kelvin Zhang <me@kelvinzhang.ca>
2023-11-29 13:41:39 -0500

    fix build

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,7 @@
-import { resolve } from "path";
-
 import { defineConfig } from "vite";
 import { comlink } from "vite-plugin-comlink";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  alias: {
-    "~": resolve(__dirname, "src"),
-  },
   plugins: [comlink()],
-  worker: {
-    plugins: [comlink()],
-  },
 });
